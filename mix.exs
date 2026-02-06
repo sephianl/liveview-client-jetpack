de8988dd1cb84f41445dacdacae9653e130be3fc
daniel <daniel@sephia.io>
2026-02-06 21:39:42 +0100

    use pinetops/live_view_native

--- mix.exs
+++ mix.exs
@@ -31,10 +31,10 @@ defmodule LiveViewNative.Jetpack.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:floki, ">= 0.30.0", only: :test},
-      {:live_view_native, github: "liveview-native/live_view_native", branch: "main", override: true},
+      {:live_view_native, github: "pinetops/live_view_native", branch: "fix-template-position-collision", override: true},
       {:live_view_native_stylesheet, github: "liveview-native/live_view_native_stylesheet", branch: "main", only: :test},
       {:live_view_native_live_form, github: "liveview-native/liveview-native-live-form", only: :test},
-      {:live_view_native_test_endpoint, github: "liveview-native/live_view_native_test_endpoint", branch: "main", only: :test},
+      # {:live_view_native_test_endpoint, github: "liveview-native/live_view_native_test_endpoint", branch: "main", only: :test},
       {:gettext, "~> 0.20", only: :test},
       {:nimble_parsec, "~> 1.3"}
     ]
